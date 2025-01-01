654e7a4fc99f05e3085acd522e43494ef1f1a82b
Luca Henrique Tavares <160362413+Luca-Henrique-Tavares@users.noreply.github.com>
2025-01-01 17:07:57 -0300

    Lógica Fuzzy -> Normalização
    Mudança e criação de normalização no código

--- troca_de_termos.py
+++ troca_de_termos.py
@@ -2,6 +2,12 @@ import pandas as pd
 from sqlalchemy import create_engine
 from thefuzz import fuzz
 import numpy as np
+from unidecode import unidecode
+import re
+
+t1 = 0
+t2 = 0
+t3 = 0
 
 #Conexão com o banco de dados
 def connection():
@@ -10,6 +16,31 @@ def connection():
         return engine
     except Exception as e:
         print(e)
+
+#Função que normaliza os termos para que possamos compará-los
+def normalizar_termos1(text):
+    global t1
+    # Remove acentos
+    texto_sem_acentos = unidecode(text)
+    # Remove espaços, hífens e transforma em minúsculo
+    t1 = re.sub(r'\s+|-', '', texto_sem_acentos).lower()
+   
+ 
+
+def normalizar_termos2(text):
+    global t2
+    # Remove acentos
+    texto_sem_acentos = unidecode(text)
+    # Remove espaços, hífens e transforma em minúsculo
+    t2 = re.sub(r'\s+|-', '', texto_sem_acentos).lower()
+
+
+def normalizar_termos3(text):
+    global t3
+    # Remove acentos
+    texto_sem_acentos = unidecode(text)
+    # Remove espaços, hífens e transforma em minúsculo
+    t3= re.sub(r'\s+|-', '', texto_sem_acentos).lower()
 
 #Função que lê as tabelas do banco de dados e compara caso haja semelhança entre os termos para que possamos adcionar a chave estrangeira
 #OBS: Se a semelhança for maior que 83% o termo será adcionado
@@ -28,27 +59,44 @@ def ler_tabelas():
     f = 0
     cont=0
     while f < len(df_sus):
-        medida = 83
+        medida = 0
         titulo1 = 'Erro'
         titulo2 = 'Erro'
         for i in range(0,len(df_dici)):
-                if fuzz.ratio(df_dici['title'].iloc[i],df_sus['titulo'].iloc[f]) > medida:
-                    medida = fuzz.ratio(df_dici['title'].iloc[i],df_sus['titulo'].iloc[f])
-                    titulo1 = df_dici['title'].iloc[i]
-                    titulo2 = df_sus['titulo'].iloc[f]
+                global t1
+                global t2
+                global t3
+                t1 = df_sus['titulo'].iloc[f]
+                t2 = df_dici['title'].iloc[i]
+                normalizar_termos1(t1)
+                normalizar_termos2(t2)
+                if fuzz.ratio(t1,t2) > medida:
+                    medida = fuzz.ratio(t1,t2)
+                    titulo1 = t1
+                    titulo2 = t2
                     id = df_dici['id_true'].iloc[i]
+                if df_dici['also-called'].iloc[i] is not None:
+                    t3 = df_dici['also-called'].iloc[i]
+                    normalizar_termos3(t3)
+                if fuzz.ratio(t1,t3) > medida:
+                    medida = fuzz.ratio(t3,t1)
+                    titulo1 = t1
+                    titulo2 = t3
+                    t3 = None
+                    id = df_dici['id_true'].iloc[i]
+              
         if(medida > 83):
             # Se a medida for maior que 83, atribua o id da tabela dici_teste à nova coluna. Reparece que em ambas recebem o valor de sua linha e coluna. Isso funcionará como uma chave estrangeira em nosso código
             df_sus.loc[f, 'dici_teste_id'] = id
         #Adciona a nova coluna ao código
         df_sus.to_sql('sus2',con=connection(),if_exists='replace',index=False)
         if(titulo1 != 'Erro' and titulo2 != 'Erro'):
-            print(titulo1,id)      
+            print(titulo1,'<-->',titulo2 , 'Medida:',medida)      
             cont+=1
         f+=1
     print('Total de termos encontrados:',cont)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     connection()
     ler_tabelas()
     
